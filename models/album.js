c0659cc362af2abae554592ff53cbaf3824d989b
CaptMerica <rmerica@live.com>
2023-01-16 18:10:04 -0500

    add review model

--- models/album.js
+++ models/album.js
@@ -4,9 +4,13 @@ const Schema = mongoose.Schema
 
 const albumSchema = new Schema({
   name: String,
+  reviews: [reviewSchema],
+  rating: Number,
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+}, {
   bestSong: String,
-  review: [reviewSchema],
-  
+}, {
+  timestamps: true,
 })
 
 const Album = mongoose.model('Album', albumSchema)
