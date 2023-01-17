e636f4438ec1bcd99028d9925a39c1f11f94d240
CaptMerica <rmerica@live.com>
2023-01-16 21:18:29 -0500

    add albums router

---
+++ routes/albums.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as albumsCtrl from '..controllers/albums.js'
+
+const router = Router()
+
+// GET localhost:3000/albums
+router.get('/albums', albumsCtrl.new)
+
+export {
+  router
+}
