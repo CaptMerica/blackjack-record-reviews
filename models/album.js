eee545f06a8a17c86ba41bae3bf5c26886487dc7
CaptMerica <rmerica@live.com>
2023-01-16 19:37:50 -0500

    attach Azure Database

--- models/album.js
+++ models/album.js
@@ -1,6 +1,13 @@
-import mongoose, { isObjectIdOrHexString } from "mongoose";
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema
+
+const reviewSchema = new Schema ({
+  content: String,
+  rating: {type: Number, min: 1, max: 5, default: 5}
+}, {
+  author: {type: Schema.Types.ObjectId, ref: "Profile"}
+})
 
 const albumSchema = new Schema({
   name: String,
